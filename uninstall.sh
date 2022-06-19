5dfe5ba0819fc655ba7574435de6156e1a0548e1
abdelrahman desouky <abdelrahman.desoky.3@gmail.com>
2022-06-19 20:22:48 +0200

    initial

---
+++ uninstall.sh
@@ -0,0 +1,11 @@
+helm uninstall emailservice
+helm uninstall cartservice
+helm uninstall currencyservice
+helm uninstall paymentservice
+helm uninstall productcatalogeservice
+helm uninstall recommendationservice
+helm uninstall shippingservice
+helm uninstall adservice
+helm uninstall checkoutservice
+helm uninstall frontendservice
+helm uninstall redis-cart
